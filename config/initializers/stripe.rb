6911347aea1a55be27b7d61209e43d8124cf9b9f
Amit Kumar Sharma <sharma_ami@hcl.com>
2017-03-07 01:48:03 +0530

    Env variable added for stripe configurations.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,2 @@
-Stripe.api_key = "sk_test_SvJwBeH7pYoHM2lREVNkHTbG"
-STRIPE_PUBLIC_KEY = "pk_test_Wvy1Uyu7XyRZ6KFXS9McHj3Z"
+Stripe.api_key = ENV["STRIPE_API_KEY"]
+STRIPE_PUBLIC_KEY =  ENV["STRIPE_PUBLIC_KEY"]
